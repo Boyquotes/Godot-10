5b35b465d58f2f16de412e4d2c2cb28fd853554a
Young Jae Ahn <youngjaeahn@Youngs-MacBook-Pro.local>
2019-12-07 17:53:27 -0600

    working

--- Planet Jumper/scripts/Planet.gd
+++ Planet Jumper/scripts/Planet.gd
@@ -5,7 +5,22 @@ signal planet_position
 
 var speed = 200
 var velocity = Vector2()
+var player_attached = false
 	
 func _physics_process(delta):
 	emit_signal("planet_velocity", get_linear_velocity())
 	emit_signal("planet_position", get_global_position())
+	
+func _process(delta):
+	var colliding_bodies = get_colliding_bodies()
+	check_player_attached(colliding_bodies)
+
+func check_player_attached(colliding_bodies):
+	if (colliding_bodies && !player_attached):
+		for i in range(0, colliding_bodies.size()):
+			if (colliding_bodies[i].name == "Player"):
+				player_attached = true
+
+func check_player_detached(player_is_jumping):
+	if (player_is_jumping):
+		player_attached = false
