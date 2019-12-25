6132a6c66b858d7a3704bda40b26a850bc85357f
Young Jae Ahn <youngjaeahn@Youngs-MacBook-Pro.local>
2019-12-24 22:50:51 -0600

    dank memes

--- Planet Jumper/scripts/Player.gd
+++ Planet Jumper/scripts/Player.gd
@@ -3,12 +3,11 @@ extends RigidBody2D
 # Variable initializations
 var is_attached = false
 var attached_planet
-var rotation_speed = 10
+var rotation_speed = 2
 var jump_speed = 50.0
 
 # Run on start up
 func _ready():
-#	self.set_linear_velocity(Vector2(100, 0))
 	pass
 	
 # Run on physics process
@@ -24,18 +23,18 @@ func player_jump(input_event, jump_speed):
 	if (input_event.is_action_pressed("ui_select") && is_attached):
 		# Make the player jump directly above
 		var jumpDirection = (self.get_global_position() - attached_planet.get_global_position()).normalized()
-		self.set_linear_velocity(jumpDirection * jump_speed) # Player will carry the momentum of the traveling planet
-		attached_planet.set_linear_velocity(-jumpDirection * jump_speed)
+		self.set_linear_velocity(jumpDirection * jump_speed)
+		
+		# Planet rebounds on the opposite direction based on planet's mass
+		self.mass = 1.00
+		attached_planet.linear_velocity = (attached_planet.get_linear_velocity()*attached_planet.mass - self.get_linear_velocity()*self.mass) / attached_planet.mass
 		
 		is_attached = false
-		self.gravity_scale = 1.0 # TODO player is affected by the attached_planet's gravity before it gets out of its gravity field range....
-		# Maybe instead of changing gravity_scale, disable the gravity of the planet?
-		### TODO make it so that I can disable the gravity of the attached_planet
-		attached_planet.get_child(2).gravity = 0 # Problem. Don't know how to turn it back on. Fuck!
+		self.gravity_scale = 1.0
+		attached_planet.get_child(2).set_collision_layer_bit(0, false)
 
 # 1. Move the player around the planet. 2. Make sure the player is oriented correctly while rotating. 3. Let the velocity of player match the planet when position is switched due to rotation
 func rotate_player(delta):
-	# print("is_attached: ", is_attached)
 	if (is_attached):
 		var planet_position = attached_planet.get_global_position()
 		var planet_velocity = attached_planet.get_linear_velocity()
@@ -71,11 +70,16 @@ func rotate_around(origin, player_pos, angle):
 
 # When player collides with other objects
 func _on_Player_body_entered(body):
+	# Reenable the gravity of planet that the player jumped away from...
 	if (attached_planet):
-		attached_planet.get_child(2).gravity = 150
+		if (attached_planet.name != body.name):
+			attached_planet.get_child(2).set_collision_layer_bit(0, true)
+			self.mass = 0.01
+			self.gravity_scale = 0.0 # Prevents the player from being affected by gravity
+			attached_planet = body # Where attached planet data is derived
+			is_attached = true # Simple indicator whether player has collided with a planet
 	
 	if (!is_attached):
-		# print("Planet running")
 		self.gravity_scale = 0.0 # Prevents the player from being affected by gravity
 		attached_planet = body # Where attached planet data is derived
 		is_attached = true # Simple indicator whether player has collided with a planet
