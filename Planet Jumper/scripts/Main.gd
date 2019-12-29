947d99160e502e36a4937209923bf9fda9b660e3
danii956 <danii956@yahoo.com>
2019-12-28 21:21:56 -0600

    dank

--- Planet Jumper/scripts/Main.gd
+++ Planet Jumper/scripts/Main.gd
@@ -1,5 +1,13 @@
 extends Node
 
+""" 
+TODO:
+	1. Create a HUD scene to display for gameover
+	2. Implement a Hit signal for Player.gd when it hits the enemy
+	3. Connect game_over() with hit signal
+	4.  Make enemies 'disappear' when it collides with a planet
+""" 
+
 # Run when enemy collides with player
 func game_over():
 	pass
